b34f52c15cbd9172268edb33f346a36864d022ff
Rone Arruda <ronearruda04@gmail.com>
2023-04-09 00:09:55 -0300

    js

--- js/diava.js
+++ js/diava.js
@@ -1,4 +1,26 @@
 
 
+const horas = document.getElementById('horas')
+const minutos = document.getElementById('minutos')
+const segundos = document.getElementById('segundos')
+
+const relogio = setInterval(function time() {
+    let hoje = new Date();
+    let hours = hoje.getHours();
+    let minutes = hoje.getMinutes();
+    let seconds = hoje.getSeconds();
+
+    if(hours < 10 ) hours = '0' + hours
+    if(minutes < 10 ) minutes = '0' + minutes
+    if(seconds < 10 ) seconds = '0' + seconds
+
+    horas.innerHTML = hours
+    minutos.innerHTML = minutes
+    segundos.innerHTML = seconds
+
+
+
+
+})
 
 
